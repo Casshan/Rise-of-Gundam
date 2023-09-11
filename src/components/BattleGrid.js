f865e9f07c72d8a0874354e965de9c77d099212c
Cass <cass@Casss-MBP.attlocal.net>
2023-09-11 04:24:05 -0500

    made battle grid. player and enemy blocks are colored accordingly.

--- src/components/BattleGrid.js
+++ src/components/BattleGrid.js
@@ -1,12 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 
 const BattleGrid = () => {
+    const [grid, setGrid] = useState([
+        [null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, 'X', null, null, null, null],
+        [null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, 'O', null, null, null, null],
+        [null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null]
+    ]);
 
     return (
         <>
-        <div id="battle-grid">
-            Grid Map
-        </div>
+            <div id="battle-grid-container">
+                <div id="battle-grid">
+                    {grid.map((grid, index) => {
+                        return (
+                            <div key={index}>
+                                {grid.map((grid, index) => {
+                                    if (grid === 'X') {
+                                        return (
+                                            <div id="pilot-block" key={index}>{grid}</div>
+                                        )
+                                    } else if (grid === 'O') {
+                                        return (
+                                            <div id="enemy-block" key={index}>{grid}</div>
+                                        )
+                                    } else {
+                                        return (
+                                            <div id="block" key={index}>{grid}</div>
+                                        )
+                                    }
+
+                                })}
+                            </div>
+
+                        )
+                    })}
+                </div>
+            </div>
         </>
     )
 }
