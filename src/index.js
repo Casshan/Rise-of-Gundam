0e8268f8c38215825a96eafefa4be98a6010d58e
Cass <cass@Casss-MBP.attlocal.net>
2023-09-10 22:58:54 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import { createRoot } from "react-dom/client";
+
+const App = () => {
+
+    return (
+        <>
+            <h1>Rise of Gundam</h1>
+        </>
+    )
+}
+
+const root = createRoot(document.getElementById('root'));
+
+root.render(
+    <App />
+)
